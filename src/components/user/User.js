06303ffcf8ba6ff4d9bcfdf1750fb18e739bcb41
baluxxignat <aleksandr.pravo@gmail.com>
2021-06-29 12:01:12 +0300

    center

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,43 +1,24 @@
 import './User.css'
 import {useState} from "react";
-import {getUser} from "../api/API";
 
-// const Openinfo = ({phone}) => {
-//     return (<div> {phone}</div>)
-// }
+const Openinfo = ({phone}) => {
+    return (<div> {phone}</div>)
+}
 
 export default function User({inn}) {
-
-    // let [toggle, setToggle] = useState(false);
-    // const onToggle = () => setToggle(!toggle);
-
-
-
-    let [user, setUser] = useState(null);
-
-    // console.log(user);
-
-
-    const onGetUser = (userId) => {
-        getUser(userId).then(value => setUser(value.data))
-    }
-
+    let [toggle, setToggle] = useState(false);
+    const onToggle = () => setToggle(!toggle);
 
     return (
         <div className={'wrap'}>
             <div> {inn.id} - {inn.name}</div>
             <div>
-                <button onClick={() =>
-                    onGetUser(inn.id) }>
-                    OPEN INFO</button>
+                <button onClick={onToggle }>OPEN DETAILS</button>
             </div>
-            {/*{toggle && <Openinfo phone={inn.phone}/>}*/}
-            {
-                user && <div>
-                    {user.email} - {user.phone}
-                    <button>OPEN POSTS</button>
-                </div>
-            }
+            {toggle && <div className={'wrap'}>
+                <Openinfo phone={inn.phone}/>
+                <button>OPEN POSTS</button>
+            </div>}
         </div>
     );
 }
