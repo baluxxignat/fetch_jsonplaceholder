06110831c66fe610073d31a45c81da927b56ee76
baluxxignat <aleksandr.pravo@gmail.com>
2021-07-02 01:01:05 +0300

    starting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Link, Route, withRouter, Switch} from "react-router-dom";
+import Users from "./components/Users";
 
-function App() {
+export default function App() {
   return (
-      <div>
-        hello okten
+      <Router>
+          <div>
+              <Link to={'/users'}>to user page</Link>
+              <br/>
+              <Link to={'/posts'}>to post page</Link>
+              <br/>
+              <Link to={'/comments'}>to comments page</Link>
+              <br/>
 
-      </div>
+              <Route path={'/users'} render={() => <Users/>}/>
+
+          </div>
+      </Router>
 
   );
 }
 
-export default App;
+
