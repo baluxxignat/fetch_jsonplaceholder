634974175a2aa66a7f3ddd0e3725a188e3cf2ddc
baluxxignat <aleksandr.pravo@gmail.com>
2021-07-01 23:34:45 +0300

    finished exampl

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import {useState} from "react";
+import {useReducer} from "react";
 
-
-function App() {
-
-    // let [a, setA] = useState(0);
-    // let [b, setB] = useState(0);
-
+function  reducer (state, action) {               // не меняются названия
+    switch (action.type) {
+        case  'INCREMENT_A':
+            return {...state, a: state.a + action.payload};   //{a:0, b:0} click {a:1, b:0}
+        case  'INCREMENT_B':
+            return {...state, b: state.b + action.payload};
+        default:
+            return {...state};
+    }
+}
+export default function App() {
+  let [state, dispatch] = useReducer(reducer,{ a:0, b:0})              //let {a, b} = state;
 
   return (
-      // <div>
-      //     <button onClick={() => {setA(++a)}}>inc a - {a}</button>
-      //     <br/>
-      //     <button onClick={() => {setB(++b)}}>inc b - {b}</button>
-      // </div>
-
     <div>
 
-
-
+        <button onClick={() => {dispatch({type: 'INCREMENT_A', payload: 1});}}>inc a - {state.a}</button>
+        <br/>
+        <button onClick={() => {dispatch({type: 'INCREMENT_B', payload: 1});}}>inc a - {state.b}</button>
     </div>
-
   );
 }
 
-export default App;
+
