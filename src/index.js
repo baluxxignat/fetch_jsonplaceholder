970bfed700e26436cf7cea980fc30d796ba17d8b
baluxxignat <aleksandr.pravo@gmail.com>
2021-07-06 10:13:06 +0300

    Redux lection

--- src/index.js
+++ src/index.js
@@ -30,9 +30,11 @@ const store = createStore(counterReducer);
 
 ReactDOM.render(
   <React.StrictMode>
+
       <Provider store={store}>
           <App/>
       </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
