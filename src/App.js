4488beaa5a382cf01aad9949da3a6a32fc1651ca
baluxxignat <aleksandr.pravo@gmail.com>
2021-07-12 15:02:09 +0300

    Redux 1 users button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import {useSelector, useDispatch} from "react-redux";
 import './App.css';
+import {useEffect, useState} from "react";
+
 
 const Child = () => {
     const counter = useSelector((state) => state.counterValue);
     //console.log(counter);
+    const posts = useSelector((state) => state.posts);
     return (
         <div >
             <h1>{counter}</h1>
+            {
+                posts.map(post => (
+                    <div key={post.id}>
+                        <p>{post.url}</p>
+
+                    </div>
+                ))
+            }
         </div>
     )
 }
 
 function App() {
     const dispatch = useDispatch();
-    let input = () => {
-      return document.querySelector('input').value;
-    }
+
+    let [posts, setPost] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+            .then(response => response.json())
+            .then(response => {
+                setPost([...response])
+                //console.log(posts);
+            });
+
+    }, []);
+
+    dispatch ({
+        type: 'SET_POST',
+        payload: posts,
+    })
 
     return (
 
         <div >
-
 
             <Child/>
 
@@ -41,15 +65,15 @@ function App() {
                 </button>
             </div>
 
-            <div>
+            {/*<div>*/}
 
-                <input/>
+            {/*    <input/>*/}
 
-                <button onClick={() => {
-                    dispatch({type: 'CASTUM', payload: input})
-                }}>YOUR NUMBER
-                </button>
-            </div>
+            {/*    <button onClick={() => {*/}
+            {/*        dispatch({type: 'CASTUM', payload: input})*/}
+            {/*    }}>YOUR NUMBER*/}
+            {/*    </button>*/}
+            {/*</div>*/}
 
         </div>
     );
